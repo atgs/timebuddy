b414315c7eb6c82bde1025a489fe7594f496d431
fromkk <ueoka@fromkk.info>
2015-06-01 21:26:55 +0900

    Q & A追加

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function($)
+{
+    $(function()
+    {
+        $('.q-and-a a').on('click', function(e)
+        {
+            e.preventDefault();
+            return false;
+        })
+    });
+})(jQuery);
